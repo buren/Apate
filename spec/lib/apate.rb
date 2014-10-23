816fec18fcf6396b897765484e62c78ba54c7728
Albin Svensson <albin.svensson@trialbee.com>
2014-10-23 23:24:13 +0200

    added test files

---
+++ spec/lib/apate.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Apate do
+
+  it 'Should be able to compute the average of an array' do
+    arr = [1,2,3,4,5]
+    res = Apate.compute_average(arr)
+    expect(res).to eq(3)
+  end
+
+end
